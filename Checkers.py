e50eb559967200de425fdcbcbe80ff9600f30fce
Adam <adamvinch@gmail.com>
2020-04-07 12:35:14 -0400

    Created a game class for the main game loop, a function to setup both sides game pieces, and a function to draw out both sides game pieces

--- Checkers.py
+++ Checkers.py
@@ -16,29 +16,90 @@ LIGHT_BROWN = (255,222,173)
 
 screen = pygame.display.set_mode((600, 600))
 
-class Board:
-    board = []
+class Game:
+    def __init__(self, board):
+        self.board = board
+        self.player1 = Player(0)
+        self.player2 = Player(0)
+        self.player1, self.player2 = self.board.setupPieces(self.player1, self.player2)
+        print(self.player2.tiles)
+        print(self.player1.tiles)
+    def play(self):
+        screen.fill((LIGHT_BROWN))
+        draw.drawBoard(screen, WIDTH, HEIGHT)
+        pygame.display.update()
 
-    def resetBoard():
+        self.board.printBoard()
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    break
+
+            draw.updateBoard(screen, WIDTH, HEIGHT, self.board)
+            pygame.display.update()
+
+class Board:
+    def __init__(self):
+        # creates list of 8x8 to represent game board
+        self.board = self.createBoard()
+    def createBoard(self):
         board = []
         for row in range(8):
             row = []
-            for column in range(8):
+            for j in range(8):
                 row.append('')
             board.append(row)
+        return board
+
+    def setupPieces(self, player1, player2):
+        board = self.board
+
+        # set up red pieces (player2)
+        for rowIndex, row in enumerate(self.board):
+            for columnIndex, column in enumerate(row):
+                if (rowIndex == 0 or rowIndex == 2) and player2.tiles <= 12:
+                    if columnIndex % 2 == 0:
+                        self.board[rowIndex][columnIndex] = 'X'
+                        player2.tiles += 1
+                elif rowIndex == 1:
+                    if columnIndex % 2 != 0:
+                        self.board[rowIndex][columnIndex] = 'X'
+                        player2.tiles += 1
+
+        # set up white pieces (player1)
+
+        for rowIndex, row in enumerate(reversed(self.board)):
+            for columnIndex, column in enumerate(reversed(row)):
+                if rowIndex == len(self.board) - 1 or rowIndex == len(self.board) - 3:
+                    if player2.tiles <= 12:
+                        if columnIndex % 2 != 0:
+                            self.board[rowIndex][columnIndex] = 'O'
+                            player1.tiles += 1
+                elif rowIndex == len(self.board) - 2:
+                    if columnIndex % 2 == 0:
+                        self.board[rowIndex][columnIndex] = 'O'
+                        player1.tiles += 1
+        return player1, player2
 
 
 
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            break
+    def printBoard(self):
+        for row in self.board:
+            for index, space in enumerate(row):
+                if index != 0:
+                    print(f'{space} | ', end='')
+                elif index == 0:
+                    print(f'| {space} | ', end='')
+            print()
+            print('-' * 25)
 
-    Board.printBoard()
-    input()
+class Player:
+    def __init__(self, tiles):
+        self.tiles = tiles
 
-    screen.fill((LIGHT_BROWN))
-    draw.drawBoard(screen, WIDTH, HEIGHT)
-    pygame.display.update()
+gameBoard = Board()
+game = Game(gameBoard)
+game.play()
--- DrawBoard.py
+++ DrawBoard.py
@@ -6,3 +6,27 @@ def drawBoard(WINDOW, WIDTH, HEIGHT):
 
     for count, row in enumerate(range(9)):
         pygame.draw.line(WINDOW, (0,0,0), (0, count * 75), (WIDTH, count * 75))
+
+def updateBoard(WINDOW, WIDTH, HEIGHT, board):
+    for chipRow, row in enumerate(board.board):
+        for chipColumn, space in enumerate(row):
+            if space != '':
+                if space == 'X':
+                    drawChip(WINDOW, chipColumn, chipRow,  'player1')
+                else:
+                    drawChip(WINDOW, chipColumn, chipRow, 'player2')
+
+
+def drawChip(WINDOW, chipX, chipY, player):
+    RED = (255,0,0)
+    WHITE = (255,255,255)
+    if player == 'player1':
+        color = RED
+    else:
+        color = WHITE
+
+    X = (chipX * 75) + 37
+    Y = (chipY * 75) + 37
+
+    pygame.draw.circle(WINDOW, color, (X , Y), 35)
+    pygame.display.update()
